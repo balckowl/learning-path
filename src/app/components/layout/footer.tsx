ec5b72fbaa52be008f97244bbb6224b6d4d29c16
NautilusCode <xzhouzhi@gmail.com>
2024-08-24 13:53:15 +0900

    feat: ヘッダーとフッターの作成

---
+++ src/app/components/layout/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div className="bg-slate-200 p-2 text-center">
+      <p>© created by 2024 Tech Path</p>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import Footer from "./components/layout/footer";
+import Header from "./components/layout/header";
+
 export default function Home() {
-  return <main className="flex min-h-screen flex-col items-center justify-between p-24">hello</main>;
+  return (
+    <main>
+      <div className="flex min-h-screen flex-col">
+        <Header />
+        <div className="grow">てすと</div>
+        <Footer />
+      </div>
+    </main>
+  );
 }
