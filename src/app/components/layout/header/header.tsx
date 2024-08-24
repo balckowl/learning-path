35d1d3af46610a3c0c7c132b5daee5ed88c72cf8
balckowl <shiggyjr1492@gmail.com>
2024-08-24 15:14:21 +0900

    feat: next-authでgithub認証作ったよ

--- src/app/components/layout/header/header.tsx
+++ src/app/components/layout/header/header.tsx
@@ -1,11 +1,11 @@
-import { Button } from "@/components/ui/button";
+import LoginBtn from "./login-btn";
 
 export default function Header() {
   return (
     <header className="bg-slate-200 text-4xl">
       <div className="container flex items-center justify-between py-2">
         <h1 className="font-bold">Tech Path</h1>
-        <Button className="bg-[#67A637]">ログイン</Button>
+        <LoginBtn />
       </div>
     </header>
   );
