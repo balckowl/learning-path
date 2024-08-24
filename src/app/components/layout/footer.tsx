a5f80298a71cb6195b5e3b02467bbdc6c33237b2
Sora_339 <xzhouzhi@gmail.com>
2024-08-24 14:29:20 +0900

    ヘッダーとフッターの作成 (#5)
    * feat: ヘッダーとフッターの作成
    
    * fix: ヘッダーとフッターの修正
    
    * fix: 引き継ぎ箇所の修正
    
    ---------
    
    Co-authored-by: Soma Sekine <a8121151@aoyama.jp>

---
+++ src/app/components/layout/footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-slate-200">
+      <div className="container py-2 text-center">
+        <p>© created by 2024 Tech Path</p>
+      </div>
+    </footer>
+  );
+}
