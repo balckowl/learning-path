3c14a895072b321d1634a6f012e33bb099725546
y_ta <129815120+balckowl@users.noreply.github.com>
2024-08-25 14:59:29 +0900

    style: スタイルを調整 (#16)

--- src/app/components/gallery/ArticleThumbnail.tsx
+++ src/app/components/gallery/ArticleThumbnail.tsx
@@ -9,14 +9,7 @@ export default function ArticleThumbnail({ article }: { article: ArticleType }) 
         {article.thumbnail.map((thumbnail) => (
           <div key={thumbnail.articleTitle} className="relative">
             <div className="rounded bg-[#FFDD81] p-2">
-              <Image
-                className="mb-[10px] w-full"
-                src={thumbnail.articleImg}
-                width={153}
-                height={78}
-                alt="記事サムネイル"
-              />
-              <p className="mx-2 w-[%] bg-gray-700 p-1 text-blue-500">{thumbnail.articleTitle}</p>
+              <Image className="w-full" src={thumbnail.articleImg} width={153} height={78} alt="記事サムネイル" />
             </div>
             <div className="absolute left-1/2 top-full h-8 w-1.5 -translate-x-1/2 bg-gray-300"></div>
           </div>
