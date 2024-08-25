fe6744526562d0c8c4a0565f5892bbd632ce685d
y_ta <129815120+balckowl@users.noreply.github.com>
2024-08-25 16:35:24 +0900

    feat: 個別記事取得apiを作成 (#19)

---
+++ src/app/api/article/[articleId]/route.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { getOgpInfo } from "@/lib/get-ogp-info";
+import prisma from "@/lib/prisma/client";
+
+export const GET = async (req: NextRequest, { params }: { params: { articleId: string } }) => {
+  const { articleId } = params;
+
+  const article = await prisma.article.findUnique({
+    include: { nodes: true },
+    where: { id: Number(articleId) },
+  });
+
+  if (!article) return NextResponse.json({}, { status: 404 });
+
+  const updatedNodes = await Promise.all(
+    article.nodes.map(async (node) => {
+      const ogp = await getOgpInfo(node.nodeUrl);
+      return {
+        ...node,
+        ogp, // OGPの画像URLを追加
+      };
+    }),
+  );
+
+  const updatedArticle = {
+    ...article,
+    nodes: updatedNodes,
+  };
+
+  return NextResponse.json(updatedArticle, { status: 200 });
+};
