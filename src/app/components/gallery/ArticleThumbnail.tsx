48d96a7c5054ffbe9bfddf98bbaa1f44b5455cee
balckowl <shiggyjr1492@gmail.com>
2024-08-25 11:21:37 +0900

    style: レスポンシブを修正

--- src/app/components/gallery/ArticleThumbnail.tsx
+++ src/app/components/gallery/ArticleThumbnail.tsx
@@ -8,15 +8,15 @@ export default function ArticleThumbnail({ article }: { article: ArticleType }) 
       <div className="relative space-y-8">
         {article.thumbnail.map((thumbnail) => (
           <div key={thumbnail.articleTitle} className="relative">
-            <div className="flex flex-col items-center rounded bg-[#FFDD81] p-2 lg:flex-row">
+            <div className="rounded bg-[#FFDD81] p-2">
               <Image
-                className="inline-block pb-2 lg:pb-0"
+                className="mb-[10px] w-full"
                 src={thumbnail.articleImg}
                 width={153}
                 height={78}
                 alt="記事サムネイル"
               />
-              <p className="mx-2 bg-gray-700 p-1 text-blue-500">{thumbnail.articleTitle}</p>
+              <p className="mx-2 w-[%] bg-gray-700 p-1 text-blue-500">{thumbnail.articleTitle}</p>
             </div>
             <div className="absolute left-1/2 top-full h-8 w-1.5 -translate-x-1/2 bg-gray-300"></div>
           </div>
