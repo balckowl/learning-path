662a8488a114454ac6dd891936f04322ba4f5698
Soma Sekine <a8121151@aoyama.jp>
2024-08-25 11:52:23 +0900

    refactor: 指摘箇所の修正

--- src/app/(page)/article/page.tsx
+++ src/app/(page)/article/page.tsx
@@ -1,6 +1,5 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
-
 import Card from "@/app/components/layout/article/card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export default function Page() {
   const cardsData = [
@@ -8,25 +7,25 @@ export default function Page() {
       id: 1,
       title: "1. Next.jsの環境構築",
       content: "まず、このページでNext.jsの環境構築とコンポーネントの知識をつけることができます。",
-      description: "これは、node-article-title1です。これは、node-article-title1です。",
       imageAlt: "Article Thumbnail",
       imageSrc: "/image/ogp.png",
+      nodeArticleTitle: "これは、node-article-title1です。これは、node-article-title1です。",
     },
     {
       id: 2,
       title: "2. TailwindCSSの学習",
       content: "次にこの記事でTailwindCSSの使い方を学ぶことができます。",
-      description: "これは、node-article-title2です。これは、node-article-title2です。",
       imageAlt: "Article Thumbnail",
       imageSrc: "/image/ogp.png",
+      nodeArticleTitle: "これは、node-article-title2です。これは、node-article-title2です。",
     },
     {
       id: 3,
       title: "3. Vercalへのデプロイ",
       content: "最後にこの記事でデプロイの方法について学ぶことができます。",
-      description: "これは、node-article-title3です。これは、node-article-title3です。",
       imageAlt: "Article Thumbnail",
       imageSrc: "/image/ogp.png",
+      nodeArticleTitle: "これは、node-article-title3です。これは、node-article-title3です。",
     },
   ];
   return (
@@ -34,7 +33,7 @@ export default function Page() {
       <div className="flex justify-center px-[10px] py-[20px]">
         <div className="w-full rounded-md bg-white p-8 md:w-[85%] xl:w-3/4 xl:px-[70px]">
           <h1 className="mb-3 text-3xl font-bold">Next.jsで簡単なWebアプリが作れるようになる</h1>
-          <p className="mb-4 text-gray-600">2024/08/24</p>
+          <time className="mb-4 text-gray-600">2024/08/24</time>
           <div className="mb-6 flex items-center justify-between">
             <div className="flex gap-1">
               <p className="size-fit rounded-full bg-slate-400 p-1 px-3">Next.js</p>
@@ -58,7 +57,7 @@ export default function Page() {
                   title={card.title}
                   imageSrc={card.imageSrc}
                   imageAlt={card.imageAlt}
-                  description={card.description}
+                  nodeArticleTitle={card.nodeArticleTitle}
                   content={card.content}
                 />
               ))}
--- src/app/components/layout/article/card.tsx
+++ src/app/components/layout/article/card.tsx
@@ -5,12 +5,12 @@ import Image from "next/image";
 interface CardProps {
   title: string;
   content: string;
-  description: string;
   imageAlt: string;
   imageSrc: string;
+  nodeArticleTitle: string;
 }
 
-export default function Card({ title, content, description, imageAlt, imageSrc }: CardProps) {
+export default function Card({ title, content, imageAlt, imageSrc, nodeArticleTitle }: CardProps) {
   return (
     <div className="mx-auto rounded-lg bg-yellow-100 p-6 shadow-md">
       <h3 className="mb-4 text-lg font-semibold">{title}</h3>
@@ -22,7 +22,7 @@ export default function Card({ title, content, description, imageAlt, imageSrc }
           height={200}
           className="mb-[10px] mr-4 w-full md:mb-0 md:w-[50%]"
         />
-        <p className="w-full text-xl md:w-[50%]">{description}</p>
+        <p className="w-full text-xl md:w-[50%]">{nodeArticleTitle}</p>
       </div>
       <p className="mt-4 text-gray-700">{content}</p>
     </div>
