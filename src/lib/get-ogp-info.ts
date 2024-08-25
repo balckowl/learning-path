9def5607687fc2b4b90e10b252407517606fdf36
Somahc <131226104+Somahc@users.noreply.github.com>
2024-08-25 15:14:37 +0900

    OGP情報取得関数の作成 (#15)
    * feat: ogp情報取得関数作成
    
    * refactor: console出力の調整

---
+++ src/lib/get-ogp-info.ts
@@ -0,0 +1,28 @@
+import { JSDOM } from 'jsdom';
+
+export const getOgpInfo = async (url: string) => {
+    const proxyUrl = `https://corsproxy.io/?${encodeURIComponent(url)}`;
+    const response = await fetch(proxyUrl);
+    const html = await response.text();
+    const domParser = new JSDOM().window.DOMParser;
+    const dom = new domParser().parseFromString(html, 'text/html');
+
+    const ogp = Object.fromEntries(
+        [...dom.head.children]
+            .filter(
+                (element) =>
+                    element.tagName === 'META' &&
+                    element.getAttribute('property')?.startsWith('og:')
+            )
+            .map((element) => {
+                return [
+                    element.getAttribute('property'),
+                    element.getAttribute('content')
+                ];
+            })
+    );
+
+    console.log(ogp);
+
+    return ogp;
+}
