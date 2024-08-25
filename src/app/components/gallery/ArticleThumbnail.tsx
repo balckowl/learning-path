fb2e70556c709ed9954ac4078335488737e9ac5e
Soma Sekine <a8121151@aoyama.jp>
2024-08-25 11:04:42 +0900

    feat: レスポンシブ時の記事情報縦並び

--- src/app/components/gallery/ArticleThumbnail.tsx
+++ src/app/components/gallery/ArticleThumbnail.tsx
@@ -8,8 +8,14 @@ export default function ArticleThumbnail({ article }: { article: ArticleType }) 
       <div className="relative space-y-8">
         {article.thumbnail.map((thumbnail) => (
           <div key={thumbnail.articleTitle} className="relative">
-            <div className="flex items-center rounded bg-[#FFDD81] p-2">
-              <Image className="inline-block" src={thumbnail.articleImg} width={153} height={78} alt="記事サムネイル" />
+            <div className="flex flex-col items-center rounded bg-[#FFDD81] p-2 lg:flex-row">
+              <Image
+                className="inline-block pb-2 lg:pb-0"
+                src={thumbnail.articleImg}
+                width={153}
+                height={78}
+                alt="記事サムネイル"
+              />
               <p className="mx-2 bg-gray-700 p-1 text-blue-500">{thumbnail.articleTitle}</p>
             </div>
             <div className="absolute left-1/2 top-full h-8 w-1.5 -translate-x-1/2 bg-gray-300"></div>
