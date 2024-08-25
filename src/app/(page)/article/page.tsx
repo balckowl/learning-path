fdf3b04e792f66385b4ee5d5bc36c2623cc82755
balckowl <shiggyjr1492@gmail.com>
2024-08-25 10:23:28 +0900

    style: スタイルを共通化させて、レスポンシブを修正

--- src/app/(page)/article/page.tsx
+++ src/app/(page)/article/page.tsx
@@ -30,36 +30,40 @@ export default function Page() {
     },
   ];
   return (
-    <div className="container flex min-h-screen flex-col items-center bg-gray-100 py-6">
-      <h1 className="mb-3 text-3xl font-bold">Next.jsで簡単なWebアプリが作れるようになる</h1>
-      <p className="mb-4 text-gray-600">2024/08/24</p>
-      <div className="mb-6 flex w-full max-w-4xl justify-between">
-        <div className="flex gap-1">
-          <p className="size-fit rounded-full bg-slate-400 p-1 px-3">Next.js</p>
-          <p className="size-fit rounded-full bg-slate-400 p-1 px-3">React</p>
-        </div>
-        <div className="flex items-center">
-          <Avatar className="mx-auto size-12">
-            <AvatarImage src="image/icon.png" className="size-12" />
-            <AvatarFallback>そらち</AvatarFallback>
-          </Avatar>
-          <h2 className="ml-2 text-lg font-semibold">Sorachi Nishibori</h2>
-        </div>
-      </div>
-      <div className="relative flex flex-col items-center">
-        {/* Vertical line connecting the cards */}
-        <div className="absolute left-1/2 z-0 h-full w-1 -translate-x-1/2 bg-gray-500"></div>
-        <div className="z-10 space-y-8">
-          {cardsData.map((card) => (
-            <Card
-              key={card.id}
-              title={card.title}
-              imageSrc={card.imageSrc}
-              imageAlt={card.imageAlt}
-              description={card.description}
-              content={card.content}
-            />
-          ))}
+    <div className="bg-yellow-300">
+      <div className="flex justify-center px-[10px] py-[20px]">
+        <div className="w-full rounded-md bg-white p-8 md:w-[85%] xl:w-3/4 xl:px-[70px]">
+          <h1 className="mb-3 text-3xl font-bold">Next.jsで簡単なWebアプリが作れるようになる</h1>
+          <p className="mb-4 text-gray-600">2024/08/24</p>
+          <div className="mb-6 flex items-center justify-between">
+            <div className="flex gap-1">
+              <p className="size-fit rounded-full bg-slate-400 p-1 px-3">Next.js</p>
+              <p className="size-fit rounded-full bg-slate-400 p-1 px-3">React</p>
+            </div>
+            <div className="flex items-center gap-3">
+              <Avatar className="mx-auto size-10">
+                <AvatarImage src="image/icon.png" className="size-10" />
+                <AvatarFallback>そらち</AvatarFallback>
+              </Avatar>
+              <h2 className="text-lg font-semibold">Sorachi Nishibori</h2>
+            </div>
+          </div>
+          <div className="relative flex flex-col items-center">
+            {/* Vertical line connecting the cards */}
+            <div className="absolute left-1/2 z-0 h-full w-1 -translate-x-1/2 bg-gray-500"></div>
+            <div className="z-10 space-y-8">
+              {cardsData.map((card) => (
+                <Card
+                  key={card.id}
+                  title={card.title}
+                  imageSrc={card.imageSrc}
+                  imageAlt={card.imageAlt}
+                  description={card.description}
+                  content={card.content}
+                />
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/app/components/layout/article/card.tsx
+++ src/app/components/layout/article/card.tsx
@@ -14,9 +14,15 @@ export default function Card({ title, content, description, imageAlt, imageSrc }
   return (
     <div className="mx-auto rounded-lg bg-yellow-100 p-6 shadow-md">
       <h3 className="mb-4 text-lg font-semibold">{title}</h3>
-      <div className="flex items-center">
-        <Image src={imageSrc} alt={imageAlt} width={300} height={200} className="mr-4" />
-        <p className="text-xl">{description}</p>
+      <div className="flex flex-col md:flex-row md:items-center">
+        <Image
+          src={imageSrc}
+          alt={imageAlt}
+          width={300}
+          height={200}
+          className="mb-[10px] mr-4 w-full md:mb-0 md:w-[50%]"
+        />
+        <p className="w-full text-xl md:w-[50%]">{description}</p>
       </div>
       <p className="mt-4 text-gray-700">{content}</p>
     </div>
