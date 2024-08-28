bcce59cd91b4da0c7e461e5759f9e1f8723683c3
balckowl <shiggyjr1492@gmail.com>
2024-08-28 22:52:13 +0900

    feat: ページネーションの作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,31 @@ import Link from "next/link";
 import Article from "@/app/components/gallery/Article";
 import { GalleryArticle } from "@/types/gallery-articles";
 
-export default async function Home() {
-  const res = await fetch("http:/localhost:3000/api/articles", { cache: "no-store" });
+import Pagination from "./components/shared/pagination";
 
-  const articleData: GalleryArticle[] = await res.json();
+export default async function Home({ searchParams }: { searchParams: { page: string } }) {
+  const page = searchParams.page || "1";
+  const res = await fetch(`http:/localhost:3000/api/articles?page=${page}`, {
+    cache: "no-store",
+  });
+
+  const articlesBytotalArticles: { articles: GalleryArticle[]; totalArticles: number } = await res.json();
+  const { articles, totalArticles } = articlesBytotalArticles;
+  const limit = 9;
+  const currentPage = parseInt(page, 10);
 
   return (
     <div className="min-h-[calc(100vh-60px-50px)] bg-yellow-300">
       <div className="container px-[10px] py-[20px]">
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
-          {articleData.map((article) => (
+        <div className="mb-[20px] grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
+          {articles.map((article) => (
             <Link href={`/article/${article.id}`} key={article.id}>
               <Article article={article} />
             </Link>
           ))}
         </div>
+        {/*pagination*/}
+        <Pagination limit={limit} currentPage={currentPage} totalCount={totalArticles} />
       </div>
     </div>
   );
