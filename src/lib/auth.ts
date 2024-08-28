4aa7b8896d937bfaf6b857d4b434ef143661c60a
potekichi <98818162+potek1ch1@users.noreply.github.com>
2024-08-28 23:01:47 +0900

    fix: nextauthoptionsを外部ファイルに切り分けた (#35)

---
+++ src/lib/auth.ts
@@ -0,0 +1,24 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { Session, User } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+import prisma from "@/lib/prisma/client";
+
+export const authOptions = {
+  adapter: PrismaAdapter(prisma),
+  callbacks: {
+    async session({ session, user }: { session: Session; user: User }) {
+      if (session?.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID ?? "",
+      clientSecret: process.env.GITHUB_CLIENT_SECRET ?? "",
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+};
