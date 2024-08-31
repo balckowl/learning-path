ebf0f2f3bc6007abd0436505bbd4a21dec7ea75e
EaGitro <97268187+EaGitro@users.noreply.github.com>
2024-08-31 22:26:13 +0900

    ea api deletearticle (#59)
    * feat: create delete endpoint
    
    * feat: create delete endpoint

--- src/app/api/article/[articleId]/route.ts
+++ src/app/api/article/[articleId]/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
+import { getServerSession } from "next-auth/next";
 
+import { authOptions } from "@/lib/auth";
 import { getOgpInfo } from "@/lib/get-ogp-info";
 import prisma from "@/lib/prisma/client";
 
@@ -29,4 +31,39 @@ export const GET = async (req: NextRequest, { params }: { params: { articleId: s
   };
 
   return NextResponse.json(updatedArticle, { status: 200 });
+};
+
+export const DELETE = async (req: NextRequest, { params }: { params: { articleId: string } }) => {
+  const session = await getServerSession(authOptions);
+  if (!session) throw Error("認証してくださいね!");
+
+  const { id: authorId } = session.user;
+  const id = Number(params.articleId);
+  if (!id) throw Error("無効なURLです");
+  const article = await prisma.article.findUnique({
+    where: {
+      id: id,
+    },
+  });
+  console.log(article);
+  if (article?.authorId != authorId) throw Error("他のユーザの記事を消そうとしています!");
+
+  const { count } = await prisma.node.deleteMany({
+    where: {
+      articleId: id,
+    },
+  });
+
+  const deleteArticle = await prisma.article.delete({
+    where: {
+      id: id,
+    },
+  });
+
+  const returnObj = {
+    deletedArticle: deleteArticle,
+    deletedNodesCount: count,
+  };
+
+  return NextResponse.json({ returnObj }, { status: 200 });
 };
