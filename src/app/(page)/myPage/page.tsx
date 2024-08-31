8a1f03749e7c5125e66d00fcb2450b3bc266140a
Sora339 <xzhouzhi@gmail.com>
2024-08-31 16:55:06 +0900

    feat: マイページの作成

---
+++ src/app/(page)/myPage/page.tsx
@@ -0,0 +1,24 @@
+import {Trash2  } from "lucide-react";
+
+export default async function Home() {
+  const titles = ["記事1のタイトル", "記事2のタイトル", "記事3のタイトル", "記事4のタイトル", "記事5のタイトル"];
+
+  return (
+    <div className="container min-h-[calc(100vh-60px-50px)] bg-yellow-300">
+      <h1 className="pt-4 text-4xl">自分の記事</h1>
+      <div className="container px-[10px] py-[20px]">
+        <div className="container mb-[20px]">
+          {titles.map((title, index) => (
+            <div key={index} className="container mb-2 flex w-full items-center justify-between rounded bg-white py-4">
+              <p className="text-2xl">{title}</p>
+              <div className="flex gap-2">
+                <Trash2 />
+                {/* <Pencil /> */}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
