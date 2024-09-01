1b57717d4d2dbf5f4b7e2d909a6f7ffab0295ab2
potekichi <hrt_ioo1279@outlook.jp>
2024-09-01 14:50:55 +0900

    feat: 本番用のdockerfileを追加

--- src/types/gallery-articles.d.ts
+++ src/types/gallery-articles.d.ts
@@ -1,37 +1,37 @@
 export type GalleryArticle = {
+  id: number;
+  title: string;
+  author: {
+    id: string;
+    name: string;
+    email: string;
+    emailVerified: string;
+    image: string;
+  };
+  authorId: string;
+  category: {
     id: number;
-    title: string;
-    author: {
-        id: string;
-        name: string;
-        email: string;
-        emailVerified: string;
-        image: string;
-    };
-    authorId: string;
-    createdAt: string;
-    category: {
-        id: number;
-        name: string;
-    };
-    nodes: Node[];
-    updatedAt: string;
+    name: string;
+  };
+  createdAt: string;
+  nodes: Node[];
+  updatedAt: string;
 };
 
 export type Node = {
-    id: number;
-    articleId: number;
-    comment: string;
-    createdAt: string;
-    nodeTitle: string;
-    nodeUrl: string;
-    ogp: {
-        "og:image": string;
-        "og:site_name": string;
-        "og:title": string;
-        "og:type": string;  
-        "og:url": string;
-    };
-    order: number;
-    updatedAt: string;
+  id: number;
+  articleId: number;
+  comment: string;
+  createdAt: string;
+  nodeTitle: string;
+  nodeUrl: string;
+  ogp: {
+    "og:image": string;
+    "og:site_name": string;
+    "og:title": string;
+    "og:type": string;
+    "og:url": string;
+  };
+  order: number;
+  updatedAt: string;
 };
