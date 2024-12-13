5c7c008d5d331448ac1f86a2b049b01290cc7215
potekichi <hrt_ioo1279@outlook.jp>
2024-12-13 22:01:46 +0900

    chore: github actionsのworkflowを修正

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,29 @@
+import path from "path";
+import { fileURLToPath } from 'url';
 /** @type {import('next').NextConfig} */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const nextConfig = {
-    images: {
-        dangerouslyAllowSVG: true,
-        remotePatterns: [
-            {
-                hostname: '**', // 全てのホスト名を許可
-                protocol: 'https',
-            },
-            {
-                hostname: '**', // 全てのホスト名を許可
-                protocol: 'http',
-            },
-        ],
-    },
-    // output: "standalone",
-    // productionBrowserSourceMaps:true,
-    // reactStrictMode: true,
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        hostname: "**", // 全てのホスト名を許可
+        protocol: "https",
+      },
+      {
+        hostname: "**", // 全てのホスト名を許可
+        protocol: "http",
+      },
+    ],
+  },
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    return config;
+  },
+  // output: "standalone",
+  // productionBrowserSourceMaps:true,
+  // reactStrictMode: true,
 };
 
 export default nextConfig;
