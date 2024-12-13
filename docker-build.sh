d09018b373e8141e34b8dc78b028ca18168311a5
potekichi <98818162+potek1ch1@users.noreply.github.com>
2024-12-13 22:18:10 +0900

    chore: github actionsのworkflowを修正 (#111)

--- docker-build.sh
+++ docker-build.sh
@@ -4,7 +4,7 @@ export $(grep -v '^#' .env | xargs)
 
 docker build \
 --build-arg NEXT_PUBLIC_SUPABASE_URL=$NEXT_PUBLIC_SUPABASE_URL \
---build-arg NEXT_PUBLIC_API_ANON_KEY=$NEXT_PUBLIC_API_ANON_KEY \
+# --build-arg NEXT_PUBLIC_API_ANON_KEY=$NEXT_PUBLIC_API_ANON_KEY \
 --build-arg DATABASE_URL=$DATABASE_URL \
 -t learning-path:1.1.1 \
 -f Dockerfile.standalone .
