13c71f566084e0349671fe07fc2a595654a8834c
balckowl <shiggyjr1492@gmail.com>
2024-12-14 00:12:12 +0900

    fix: 削除apiを修正

--- src/app/api/article/[articleId]/route.ts
+++ src/app/api/article/[articleId]/route.ts
@@ -1,71 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getServerSession } from "next-auth/next";
 
-import { authOptions } from "@/lib/auth";
-import { getOgpInfo } from "@/lib/get-ogp-info";
-import prisma from "@/lib/prisma/client";
-
-export const GET = async (req: NextRequest, { params }: { params: { articleId: string } }) => {
-  const { articleId } = params;
-
-  // セッションを取得
-  const session = await getServerSession(authOptions);
-  const userId = session?.user?.id;
-
-  console.log(session);
-
-  // 記事をデータベースから取得
-  const article = await prisma.article.findUnique({
-    include: {
-      author: true,
-      category: true,
-      nodes: true,
-      tags: {
-        include: {
-          tag: true,
-        },
-      },
-    },
-    where: { id: Number(articleId) },
-  });
-
-  if (!article) return NextResponse.json({}, { status: 404 });
-
-  // ノードのOGP情報を追加
-  const updatedNodes = await Promise.all(
-    article.nodes.map(async (node) => {
-      const ogp = await getOgpInfo(node.nodeUrl);
-      return {
-        ...node,
-        ogp, // OGPの画像URLを追加
-      };
-    }),
-  );
-
-  // タグ情報を整形
-  const tags = article.tags.map((tagRelation) => tagRelation.tag);
-
-  // いいねの情報をチェック
-  let hasLiked = false;
-  if (userId) {
-    const like = await prisma.like.findFirst({
-      where: {
-        articleId: Number(articleId),
-        userId: userId,
-      },
-    });
-    hasLiked = !!like; // いいねしていればtrue、していなければfalse
-  }
-
-  const updatedArticle = {
-    ...article,
-    hasLiked, // ユーザーがいいねしているかどうか
-    nodes: updatedNodes,
-    tags,
-  };
-
-  return NextResponse.json(updatedArticle, { status: 200 });
-};
+import { authOptions } from "@/lib/auth"; // 認証設定
+import prisma from "@/lib/prisma/client"; // prisma のインスタンスをインポート
 
 export const DELETE = async (req: NextRequest, { params }: { params: { articleId: string } }) => {
   const session = await getServerSession(authOptions);
@@ -84,6 +21,11 @@ export const DELETE = async (req: NextRequest, { params }: { params: { articleId
 
   // トランザクションで削除処理を実行
   const result = await prisma.$transaction(async (prisma) => {
+    // Like の削除
+    await prisma.like.deleteMany({
+      where: { articleId: id },
+    });
+
     // ノードの削除
     const deletedNodesCount = await prisma.node.deleteMany({
       where: { articleId: id },
